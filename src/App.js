ac128ed8f0970df924717c7dc83046f77daa6fdb
anastasiia <anastasiiakudriavtseva4@gmail.com>
2023-01-21 09:03:29 -0500

    Add more HTML CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,51 @@
 function App() {
   return (
     <div className="wrapper clear">
+
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30">Cart</h2>
+            <div className="items">
+              <div className="cartItem d-flex align-center mb-20">
+                
+                <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg">
+
+                </div>
+                <div className="mb-20 mr-30 flex">
+                  <p className="mb-5">Men shoe</p>
+                  <b>$100</b>
+                </div>
+                <img className="removeBtn" src="/img/btn-remove.svg" alt="" />
+              </div>
+
+              <div className="cartItem d-flex align-center mb-20">
+                
+                <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg">
+
+                </div>
+                <div className="mb-20 mr-30 flex">
+                  <p className="mb-5">Men shoe</p>
+                  <b>$100</b>
+                </div>
+                <img className="removeBtn" src="/img/btn-remove.svg" alt="" />
+              </div>
+
+              <ul className="cartTotalBlock">
+                <li className="d-flex">
+                  <span>Sum:</span>
+                  <div></div>
+                  <b>$200</b>
+                </li>
+                <li className="d-flex">
+                  <span>Tax 0%:</span>
+                  <div>0</div>
+                  <b></b>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img style={{ width: 40, height: 40 }} src="/img/shop.png"/>
@@ -22,9 +67,19 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
         <h1 className="mb-40">All sneakers</h1>
+        <div className="search-block d-flex">
+          <img style={{width: 22, height: 25}} src="/img/search.svg" alt="Search" />
+          <input placeholder="Search..." />
+
+        </div>
+        </div>
       <div className="d-flex">
         <div className="card">
+          <div className="favorite">
+            <img src="/img/heart.svg" alt="Unliked" />
+          </div>
           <img style={{width: 133, height: 112}} src="/img/sneakers/1.jpg" alt=""/>
           <h5>Men shoes</h5>
           <div className="d-flex justify-between align-center">
